230a851a93e53759d26e892958f73a5624460b0b
Enrique <kikeporras10@gmail.com>
2018-09-06 07:19:18 -0500

    user model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'; 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // Animations
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+
 // Material
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './material';
+
+import { AuthGuard } from './_guards/auth-guard.guard';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
 
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule,
+    HttpModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MaterialModule,
+    routing,
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
